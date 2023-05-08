ecd790db4ff11b4adae7cafb031b18dd9367097a
İsmail Harmanda <iharmanda@hotmail.com>
2023-05-08 23:43:47 +0300

    creat Dragon class and inherit from Enemy class

--- ClassesDemo/main.swift
+++ ClassesDemo/main.swift
@@ -5,7 +5,13 @@
 //  Created by ismail harmanda on 8.05.2023.
 //
 
-import Foundation
+let skeleton = Enemy()
 
-print("Hello, World!")
+let dragon = Dragon()
+dragon.wingSpan=5
+dragon.attackStrength=15
+dragon.move()
+dragon.attack()
+dragon.talk(speech: "I am a dragon")
+
 
